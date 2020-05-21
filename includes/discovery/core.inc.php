862d0cb7073b0c5e47eb10634084f50e744a161c
Adam Bishop <adam@omega.org.uk>
2020-05-21 04:48:24 +0100

    asoij

--- includes/discovery/core.inc.php
+++ includes/discovery/core.inc.php
@@ -4,14 +4,14 @@ use LibreNMS\Config;
 use LibreNMS\OS;
 
 $snmpdata = snmp_get_multi_oid($device, ['sysName.0', 'sysObjectID.0', 'sysDescr.0'], '-OUQn', 'SNMPv2-MIB');
-
+$d[] = $snmpdata['.1.3.6.1.2.1.1.1.0'];
 $deviceModel = DeviceCache::getPrimary();
 $deviceModel->fill([
     'sysObjectID' => $snmpdata['.1.3.6.1.2.1.1.2.0'] ?? null,
     'sysName' => strtolower(trim($snmpdata['.1.3.6.1.2.1.1.5.0'] ?? '')),
     'sysDescr' => isset($snmpdata['.1.3.6.1.2.1.1.1.0']) ? str_replace(chr(218), "\n", $snmpdata['.1.3.6.1.2.1.1.1.0']) : null,
 ]);
-
+$d[] = isset($snmpdata['.1.3.6.1.2.1.1.1.0']) ? str_replace(chr(218), "\n", $snmpdata['.1.3.6.1.2.1.1.1.0']) : null;
 foreach ($deviceModel->getDirty() as $attribute => $value) {
     Log::event($value . ' -> ' . $deviceModel->$attribute, $deviceModel, 'system', 3);
     $device[$attribute] = $value; // update device array
@@ -19,7 +19,7 @@ foreach ($deviceModel->getDirty() as $attribute => $value) {
 
 // detect OS
 $deviceModel->os = getHostOS($device, false);
-
+$d[] = $deviceModel;
 if ($deviceModel->isDirty('os')) {
     Log::event('Device OS changed: ' . $deviceModel->getOriginal('os') . ' -> ' . $deviceModel->os, $deviceModel, 'system', 3);
     $device['os'] = $deviceModel->os;
@@ -28,10 +28,13 @@ if ($deviceModel->isDirty('os')) {
 }
 
 $deviceModel->save();
+$d[] = $deviceModel;
 load_os($device);
 load_discovery($device);
 $os = OS::make($device);
-die(print_r($device));
+$d[] = $device;
+
+die(print_r($d));
 echo "OS: " . Config::getOsSetting($device['os'], 'text') . " ({$device['os']})\n\n";
 
 register_mibs($device, Config::getOsSetting($device['os'], 'register_mibs', []), 'includes/discovery/os/' . $device['os'] . '.inc.php');
